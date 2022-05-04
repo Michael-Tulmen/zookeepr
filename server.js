d2c829ea71f047f54b4042fd8211bf6c1d95bd49
Michael Tulmen <michaeltulmen@gmail.com>
2022-05-03 23:33:19 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -3,7 +3,28 @@ const express = require('express');
 const app = express();
 
 function filterByQuery(query, animalsArray) {
+    //saved animals array as a filtered results
     let filteredResults = animalsArray;
+    //saved personality traits as a separate dedicated array
+    //if personality traits returns a string stick it into a new array and save it
+    let personalityTraitsArray = [];
+    if(query.personalityTraits) {
+        if(typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        //loop through each trait in the personality traits array:
+        personalityTraitsArray.forEach(trait => {
+            //check the trait against each animal in the filteredResults array
+            //essentially this is making a copy of the filteredResults array given the foreach
+            //but it is updating it for each trait in the .forEach()loop.
+            //filteredResults array will only contain the entries that contain the trait that was asked for 
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
     if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
     }
